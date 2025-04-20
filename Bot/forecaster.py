d5a60ff8bd98079039197b2b5efe94240c380956
Panshul Sharma <panshul@192.168.1.12>
2025-04-20 19:55:47 +0800

    remove problematic dependencies

--- Bot/forecaster.py
+++ Bot/forecaster.py
@@ -4,7 +4,6 @@ import numpy as np
 from numeric import get_numeric_forecast
 from binary import get_binary_forecast
 import os
-import forecasting_tools
 import requests
 from aiohttp import ClientSession, ClientTimeout, ClientError
 import json
@@ -38,36 +37,5 @@ async def binary_forecast(question_details, write=print):
 
 async def multiple_choice_forecast(question_details, write=print):
     return await get_multiple_choice_forecast(question_details, write)
-
-
-def call_exa_smart_searcher(question: str) -> str:
-    if OPENAI_API_KEY is None:
-        searcher = forecasting_tools.ExaSearcher(
-            include_highlights=True,
-            num_results=10,
-        )
-        highlights = asyncio.run(searcher.invoke_for_highlights_in_relevance_order(question))
-        prioritized_highlights = highlights[:10]
-        combined_highlights = ""
-        for i, highlight in enumerate(prioritized_highlights):
-            combined_highlights += f'[Highlight {i+1}]:\nTitle: {highlight.source.title}\nURL: {highlight.source.url}\nText: "{highlight.highlight_text}"\n\n'
-        response = combined_highlights
-    else:
-        searcher = forecasting_tools.SmartSearcher(
-            temperature=0,
-            num_searches_to_run=2,
-            num_sites_per_search=10,
-        )
-        prompt = (
-            "You are an assistant to a superforecaster. The superforecaster will give"
-            "you a question they intend to forecast on. To be a great assistant, you generate"
-            "a concise but detailed rundown of the most relevant news, including if the question"
-            "would resolve Yes or No based on current information. You do not produce forecasts yourself."
-            f"\n\nThe question is: {question}"
-        )
-        response = asyncio.run(searcher.invoke(prompt))
-
-    return response
-
 
 
